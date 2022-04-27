1e1932f03628482f15e46c6323cf0232eb420158
Jaqueline Silva <83984363+JQPURRR-BYTES@users.noreply.github.com>
2022-04-27 11:21:04 -0700

    find all

--- models/User.js
+++ models/User.js
@@ -6,57 +6,54 @@ class User extends Model {}
 
 // define table columns and configuration
 User.init(
-
+  {
     // TABLE COLUMN DEFINITIONS GO HERE
-    User.init(
-        {
-          // define an id column
-          id: {
-            // use the special Sequelize DataTypes object provide what type of data it is
-            type: DataTypes.INTEGER,
-            // this is the equivalent of SQL's `NOT NULL` option
-            allowNull: false,
-            // instruct that this is the Primary Key
-            primaryKey: true,
-            // turn on auto increment
-            autoIncrement: true
-          },
-          // define a username column
-          username: {
-            type: DataTypes.STRING,
-            allowNull: false
-          },
-          // define an email column
-          email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            // there cannot be any duplicate email values in this table
-            unique: true,
-            // if allowNull is set to false, we can run our data through validators before creating the table data
-            validate: {
-              isEmail: true
-            }
-          },
-          // define a password column
-          password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-              // this means the password must be at least four characters long
-              len: [4]
-            }
-          }
-        },
-        {
-          sequelize,
-          timestamps: false,
-          freezeTableName: true,
-          underscored: true,
-          modelName: 'user'
-        }
-);
 
-},
+    // define an id column
+    id: {
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        // this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        // instruct that this is the Primary Key
+        primaryKey: true,
+        // turn on auto increment
+        autoIncrement: true
+      },
+      // define a username column
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      // define an email column
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        // there cannot be any duplicate email values in this table
+        unique: true,
+        // if allowNull is set to false, we can run our data through validators before creating the table data
+        validate: {
+          isEmail: true
+        }
+      },
+      // define a password column
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          // this means the password must be at least four characters long
+          len: [4]
+        }
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+    }
+)
 
   {
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
@@ -64,14 +61,13 @@ User.init(
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
+    timestamps= false,
     // don't pluralize name of database table
-    freezeTableName: true,
+    freezeTableName= true,
     // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
+    underscored= true,
     // make it so our model name stays lowercase in the database
-    modelName: 'user'
-  }
-);
+    modelName= 'user'
+  };
 
 module.exports = User;
